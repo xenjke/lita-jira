89dfba1c6b857c63074b236738ff081e674b74a2
Andrey Shulzhenko <imnot.xen@gmail.com>
2017-01-25 17:08:49 +0300

    rubocop offences

--- spec/lita/handlers/regex_spec.rb
+++ spec/lita/handlers/regex_spec.rb
@@ -1,26 +1,15 @@
 require 'spec_helper'
 
 describe Lita::Handlers::JiraUtility, lita_handler: true do
-
-  PROJECT_PATTERN = JiraHelper::Regex::PROJECT_PATTERN
-  SUBJECT_PATTERN = JiraHelper::Regex::SUBJECT_PATTERN
-  SUMMARY_PATTERN = JiraHelper::Regex::SUMMARY_PATTERN
-
-  #
-  # route(
-  #        /^todo\s#{PROJECT_PATTERN}\s#{SUBJECT_PATTERN}(\s#{SUMMARY_PATTERN})?$/,
-  #
-  let(:regex) { Regexp.new(/^todo\s#{PROJECT_PATTERN}\s#{SUBJECT_PATTERN}(\s#{SUMMARY_PATTERN})?$/) }
+  let(:project_pattern) { JiraHelper::Regex::PROJECT_PATTERN }
+  let(:subject_pattern) { JiraHelper::Regex::SUBJECT_PATTERN }
+  let(:summary_pattern) { JiraHelper::Regex::SUMMARY_PATTERN }
+  let(:regex) { Regexp.new(/^todo\s#{project_pattern}\s#{subject_pattern}(\s#{summary_pattern})?$/) }
 
   it 'double qoutes around subject and summary' do
-    subject = "Any subject"
-    summary = "Any summary"
-    project = 'PRJ'
-    message = %(todo #{project} "#{subject}" "#{summary}")
-    match   = message.match(regex)
+    match = %(todo PRJ "Any subject" "Any summary").match(regex)
     expect(match['project']).to eq(project)
     expect(match['summary']).to eq(summary)
     expect(match['subject']).to eq(subject)
   end
-
 end
