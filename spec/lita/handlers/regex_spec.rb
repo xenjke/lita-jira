d8cb77f219a2666764e0ca5649911d1017d84877
Andrey <imnot.xen@gmail.com>
2017-01-25 17:14:48 +0300

    Fixing regexes to make 'todo' works (#2)
    * fixing regex to stop matching on first subject and summary quotes
    * basic test for todo issue regex
    * gemspec activesupport version lock below 5
    * 2.3.0 ruby
    * jira dependency was a mistake

---
+++ spec/lita/handlers/regex_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Lita::Handlers::JiraUtility, lita_handler: true do
+  let(:project_pattern) { JiraHelper::Regex::PROJECT_PATTERN }
+  let(:subject_pattern) { JiraHelper::Regex::SUBJECT_PATTERN }
+  let(:summary_pattern) { JiraHelper::Regex::SUMMARY_PATTERN }
+  let(:regex) { Regexp.new(/^todo\s#{project_pattern}\s#{subject_pattern}(\s#{summary_pattern})?$/) }
+
+  it 'double qoutes around subject and summary' do
+    match = %(todo PRJ "Any subject" "Any summary").match(regex)
+    expect(match['project']).to eq(project)
+    expect(match['summary']).to eq(summary)
+    expect(match['subject']).to eq(subject)
+  end
+end
